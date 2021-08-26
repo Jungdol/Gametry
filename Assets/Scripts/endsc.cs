9690ff9df3fb6053949a2819da171f978a761cf1
yoonseon <jenny031230@gmail.com>
2021-08-26 22:27:47 +0900

    end 수정

--- Assets/Scripts/endsc.cs
+++ Assets/Scripts/endsc.cs
@@ -7,7 +7,7 @@ public class endsc : MonoBehaviour
     // Start is called before the first frame update
 
 
-    void Start()
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
             GameQuit();
@@ -15,7 +15,7 @@ public class endsc : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void GameQuit()
     {
         Application.Quit();
     }
