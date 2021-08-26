93b1caece762f7a16f15092e51a5faa640f49676
yoonseon <jenny031230@gmail.com>
2021-08-26 20:53:01 +0900

    end

---
+++ Assets/Scripts/endsc.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endsc : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            GameQuit();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Application.Quit();
+    }
+}
