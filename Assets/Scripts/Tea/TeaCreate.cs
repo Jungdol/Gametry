cb8538bf57fd4582232be6896697fb9bfd4110e6
mixsimpson <88084889+mixsimpson@users.noreply.github.com>
2021-09-08 01:46:33 +0900

    Tea ingredient system
    차 재료 선택 시스템 입니다
    어으 드디어...

--- Assets/Scripts/Tea/TeaCreate.cs
+++ Assets/Scripts/Tea/TeaCreate.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class TeaCreate : MonoBehaviour
 {
+    private TeaSelect teaSelect;
 
+    public int chance = 0;
+    public bool stoping = true;
+
+    public void TeaInhibitor()
+    {
+
+    }
 }
--- Assets/Scripts/Tea/TeaSelect.cs
+++ Assets/Scripts/Tea/TeaSelect.cs
@@ -6,41 +6,77 @@ using UnityEngine.UI;
 [System.Serializable]
 public class TeaSelect : MonoBehaviour
 {
-    public TeaStatus teaStatus1;
-    public TeaStatus teaStatus2;
+    public static TeaStatus teaStatus1;
+    public static TeaStatus teaStatus2;
     public TeaMaterial teaMaterial;
 
-    int chance = 0;
+    public static int chance = 0;
+    public static bool stoping = true;
 
+    public GameObject resetBtn;
+    public GameObject makeTeaBtn;
+
+
+    //차 재료 선택 함수
     public void TeaSelects()
     {
-        if(chance == 0)
+        if(stoping)
         {
-            chance += 1;
-            teaStatus1 = new TeaStatus();
-            teaStatus1 = teaStatus1.SetTeaStatus(teaMaterial);
-            Debug.Log(teaStatus1.name);
-        }
-        else if(chance == 1)
-        {
-            Debug.Log("2로 넘어감");
-            teaStatus2 = new TeaStatus();
-            if(teaStatus1 != teaStatus2)
+            //첫번째 선택
+            if(chance == 0)
             {
-                chance += 1;
+                Debug.Log("1번째 선택");
+                chance = 1;
+                teaStatus1 = new TeaStatus();
+                teaStatus1 = teaStatus1.SetTeaStatus(teaMaterial);
+                Debug.Log(teaStatus1.name);
+            }
+            //두번째 선택
+            else if(chance == 1)
+            {
+                Debug.Log("2번째 선택");
+                teaStatus2 = new TeaStatus();
                 teaStatus2 = teaStatus2.SetTeaStatus(teaMaterial);
                 Debug.Log(teaStatus2.name);
-            }
-            else if(teaStatus1 == teaStatus2)
-            {
-                Debug.Log("이미 선택한 재료입니다");
-            }
 
+                //만약 똑같은 재료 클릭시 무효처리
+                if(teaStatus1.name == teaStatus2.name)
+                {
+                    Debug.Log("이미 선택한 재료입니다"); 
+                    teaStatus2 = null;
+                }
+                //다른 재료 클릭시 값을 저장하고 버튼을 띄움
+                else if(teaStatus1.name != teaStatus2.name)
+                {
+                    Debug.Log("차를 제작할 수 있습니다");
+                    Debug.Log("재료1 : "+ teaStatus1.name+" 재료2 : "+teaStatus2.name);
+
+                    resetBtn.SetActive(true);
+                    makeTeaBtn.SetActive(true);
+
+                    stoping = false;
+                }
+            }
         }
-        else if(chance == 2)
+        //재료를 모두 선택했다면 버튼을 눌러도 입력이 안되게 하기
+        else if(!stoping)
         {
             Debug.Log("더이상 선택할 수 없습니다.");
         }
+    }
+
+    //선택한 모든 재료를 리셋하기
+    public void TeaReset()
+    {
+        chance = 0;
+        stoping = true;
+        teaStatus1 = null;
+        teaStatus2 = null;
+        resetBtn.SetActive(false);
+        makeTeaBtn.SetActive(false);
+
+        Debug.Log("리셋!");
+        Debug.Log("재료1 :"+ teaStatus1.name+" 재료2 :"+teaStatus2.name);
 
     }
 
