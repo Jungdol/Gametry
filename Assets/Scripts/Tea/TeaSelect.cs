cf59516d817f821d4bebc17250b7ae625ac4e267
mixsimpson <88084889+mixsimpson@users.noreply.github.com>
2021-09-07 15:14:57 +0900

    test
    노트북에서 데스크탑으로 넘길 파일 입니다 아직 미완성입니다

--- Assets/Scripts/Tea/TeaSelect.cs
+++ Assets/Scripts/Tea/TeaSelect.cs
@@ -6,14 +6,42 @@ using UnityEngine.UI;
 [System.Serializable]
 public class TeaSelect : MonoBehaviour
 {
-    public TeaStatus teaStatus;
+    public TeaStatus teaStatus1;
+    public TeaStatus teaStatus2;
     public TeaMaterial teaMaterial;
+
+    int chance = 0;
 
     public void TeaSelects()
     {
-        teaStatus = new TeaStatus();
-        teaStatus = teaStatus.SetTeaStatus(teaMaterial);
-        Debug.Log(teaStatus.name);
+        if(chance == 0)
+        {
+            chance += 1;
+            teaStatus1 = new TeaStatus();
+            teaStatus1 = teaStatus1.SetTeaStatus(teaMaterial);
+            Debug.Log(teaStatus1.name);
+        }
+        else if(chance == 1)
+        {
+            Debug.Log("2로 넘어감");
+            teaStatus2 = new TeaStatus();
+            if(teaStatus1 != teaStatus2)
+            {
+                chance += 1;
+                teaStatus2 = teaStatus2.SetTeaStatus(teaMaterial);
+                Debug.Log(teaStatus2.name);
+            }
+            else if(teaStatus1 == teaStatus2)
+            {
+                Debug.Log("이미 선택한 재료입니다");
+            }
+
+        }
+        else if(chance == 2)
+        {
+            Debug.Log("더이상 선택할 수 없습니다.");
+        }
+
     }
 
 }
