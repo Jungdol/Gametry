be0459fccd779f39fc3dbc8b1810ef6e1bcdc8b4
yoonseon <jenny031230@gmail.com>
2021-09-09 23:50:59 +0900

    combination
    기 퍼즐 시스템 수정

---
+++ Assets/Scripts/Color select/ColorCombination.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorCombination : MonoBehaviour
+{
+   
+    public GameObject Color;
+    
+    public void ColorSetActive()
+    {
+        Color.SetActive(!Color.active)
+    }
+}
