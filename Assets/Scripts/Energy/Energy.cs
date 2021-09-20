417a4671a9fd49c971119de998257169ec5d50f6
mixsimpson <88084889+mixsimpson@users.noreply.github.com>
2021-09-21 03:18:16 +0900

    Energy UI and switch

---
+++ Assets/Scripts/Energy/Energy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Energy : MonoBehaviour
+{
+
+    public Image image;
+    public Sprite[] sprite;
+
+    public int Energy_num = 0;
+
+    public void nextEnergy()
+    {
+        if(Energy_num == 4)
+        {
+            Energy_num = 0;
+            image.sprite = sprite[Energy_num];
+        }
+        else
+        {
+            Energy_num += 1;
+            image.sprite = sprite[Energy_num];
+        }
+    }
+
+    public void preEnergy()
+    {
+        if(Energy_num == 0)
+        {
+            Energy_num = 4;
+            image.sprite = sprite[Energy_num];
+        }
+        else
+        {
+            Energy_num -= 1;
+            image.sprite = sprite[Energy_num];
+        }
+    }
+
+}
