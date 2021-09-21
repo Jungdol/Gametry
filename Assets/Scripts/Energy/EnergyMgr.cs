468fda381377c10bb264c4d7e552856dd04a4fa1
mixsimpson <88084889+mixsimpson@users.noreply.github.com>
2021-09-22 02:51:44 +0900

    help_Energy

--- Assets/Scripts/Energy/EnergyMgr.cs
+++ Assets/Scripts/Energy/EnergyMgr.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Energy_select : MonoBehaviour
+public class EnergyMgr : MonoBehaviour
 {
+    //스크립트 불러오기
     public Energy_switch energy1;
     public Energy_switch energy2;
 
+    public GameObject energy_ex;
+
+    public void ShowEx()
+    {
+        energy_ex.SetActive(true);
+    }
+
+    public void cleanUpEx()
+    {
+        energy_ex.SetActive(false);
+    }
+
+    //기 출력하기
     public void changeMood()
     {
         switch(Energy_switch.Energy_num)
